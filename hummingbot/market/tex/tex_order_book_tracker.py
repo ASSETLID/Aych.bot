0a3067dd4ddef333267e8d81ca1a40dae5016e72
Hazem El Agaty <hazemelagaty@gmail.com>
2019-10-24 15:53:20 +0200

    feat add order book tracker

---
+++ hummingbot/market/tex/tex_order_book_tracker.py
@@ -0,0 +1,104 @@
+import asyncio
+from collections import deque, defaultdict
+import logging
+from typing import (
+    Deque,
+    Dict,
+    List,
+    Optional
+)
+
+from hummingbot.logger import HummingbotLogger
+from hummingbot.core.data_type.order_book_tracker import (
+    OrderBookTracker,
+    OrderBookTrackerDataSourceType)
+from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
+from hummingbot.core.data_type.remote_api_order_book_data_source import RemoteAPIOrderBookDataSource
+from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.market.tex.tex_api_order_book_data_source import TEXAPIOrderBookDataSource
+from hummingbot.core.data_type.order_book import OrderBook
+from hummingbot.core.data_type.order_book_message import TEXOrderBookMessage, TEXOrderBookMessageType
+
+
+class TEXOrderBookTracker(OrderBookTracker):
+    _dobt_logger: Optional[HummingbotLogger] = None
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        if cls._dobt_logger is None:
+            cls._dobt_logger = logging.getLogger(__name__)
+        return cls._dobt_logger
+
+    def __init__(self,
+                 data_source_type: OrderBookTrackerDataSourceType = OrderBookTrackerDataSourceType.EXCHANGE_API,
+                 symbols: Optional[List[str]] = None):
+        super().__init__(data_source_type=data_source_type)
+
+        self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
+        self._order_book_snapshot_stream: asyncio.Queue = asyncio.Queue()
+
+        self._ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
+        self._data_source: Optional[OrderBookTrackerDataSource] = None
+        self._saved_message_queues: Dict[str, Deque[TEXOrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
+        self._symbols: Optional[List[str]] = symbols
+
+    @property
+    def data_source(self) -> OrderBookTrackerDataSource:
+        if not self._data_source:
+            if self._data_source_type is OrderBookTrackerDataSourceType.REMOTE_API:
+                self._data_source = RemoteAPIOrderBookDataSource()
+            elif self._data_source_type is OrderBookTrackerDataSourceType.EXCHANGE_API:
+                self._data_source = TEXAPIOrderBookDataSource(symbols=self._symbols)
+            else:
+                raise ValueError(f"data_source_type {self._data_source_type} is not supported.")
+        return self._data_source
+
+    @property
+    def exchange_name(self) -> str:
+        return "tex"
+
+    async def start(self):
+        await super().start()
+        self._order_book_trade_listener_task = safe_ensure_future(
+            self.data_source.listen_for_trades(self._ev_loop, self._order_book_trade_stream)
+        )
+        self._order_book_diff_listener_task = safe_ensure_future(
+            self.data_source.listen_for_order_book_diffs(self._ev_loop, self._order_book_diff_stream)
+        )
+        self._order_book_snapshot_listener_task = safe_ensure_future(
+            self.data_source.listen_for_order_book_snapshots(self._ev_loop, self._order_book_snapshot_stream)
+        )
+        self._refresh_tracking_task = safe_ensure_future(
+            self._refresh_tracking_loop()
+        )
+        self._order_book_snapshot_router_task = safe_ensure_future(
+            self._order_book_snapshot_router()
+        )
+
+    async def _track_single_book(self, symbol: str):
+        message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
+        order_book: OrderBook = self._order_books[symbol]
+
+        while True:
+            try:
+                message: TEXOrderBookMessage = None
+                saved_messages: Deque[TEXOrderBookMessage] = self._saved_message_queues[symbol]
+
+                # Process saved messages first if there are any
+                if len(saved_messages) > 0:
+                    message = saved_messages.popleft()
+                else:
+                    message = await message_queue.get()
+
+                if message.type is TEXOrderBookMessageType.SNAPSHOT:
+                    order_book.apply_snapshot(message.bids, message.asks, message.update_id)
+                    self.logger().debug("Processed order book snapshot for %s.", symbol)
+            except asyncio.CancelledError:
+                raise
+            except Exception:
+                self.logger().network(
+                    f"Unexpected error tracking order book for {symbol}.",
+                    exc_info=True,
+                    app_warning_msg=f"Unexpected error tracking order book. Retrying after 5 seconds."
+                )
+                await asyncio.sleep(5.0)
